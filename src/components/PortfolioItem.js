16d43982d75bb3c0534e42ae0a726bcff09916ad
Kui Ádám <74501875+adamkui@users.noreply.github.com>
2021-05-20 01:24:57 +0200

    portfolio item links and button animations

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -6,18 +6,20 @@ import { MdOpenInNew } from 'react-icons/md'
 export default function PortfolioItem({title, description, tags, url, img, gitRepo}) {
     return (
         <motion.div className='portfolio__project' variants={fadeInListItemShort}>
-            <img className='portfolio__projectThumbnail' alt={title} src={img} />
-            <a href={url} target="_blank" rel="noreferrer" className='portfolio__projectTitle'>{title}</a>
-            <p className='portfolio__projectDescription'>{description}</p>
-            <div className='portfolio__projectTagsContainer'>
-                {tags.map((tag, i) => (
-                    <div key={`tag${i}`} className='portfolio__projectTag'>{tag}</div>
-                ))}
-            </div>
-            <div className='portfolio__btnsContainer'>
-                <a href={url} target="_blank" rel="noreferrer" className='portfolio__btn portfolio__viewBtn'>View Website <MdOpenInNew className='portfolio__viewIcon' /></a>
-                {gitRepo && <a className='portfolio__btn portfolio__gitBtn' href={gitRepo} target='_blank' rel='noreferrer' >View on Github <FaGithub className='portfolio__gitIcon' /></a>}
-            </div>
+            <a href={url} target="_blank" rel="noreferrer">
+                <img className='portfolio__projectThumbnail' alt={title} src={img} />
+                <p href={url} target="_blank" rel="noreferrer" className='portfolio__projectTitle'>{title}</p>
+                <p className='portfolio__projectDescription'>{description}</p>
+            </a>
+                <div className='portfolio__projectTagsContainer'>
+                    {tags.map((tag, i) => (
+                        <div key={`tag${i}`} className='portfolio__projectTag'>{tag}</div>
+                    ))}
+                </div>
+                <div className='portfolio__btnsContainer'>
+                    <a href={url} target="_blank" rel="noreferrer" className='portfolio__btn portfolio__viewBtn'>View Website <MdOpenInNew className='portfolio__viewIcon' /></a>
+                    {gitRepo && <a className='portfolio__btn portfolio__gitBtn' href={gitRepo} target='_blank' rel='noreferrer' >View on Github <FaGithub className='portfolio__gitIcon' /></a>}
+                </div>
         </motion.div>
     )
 }
